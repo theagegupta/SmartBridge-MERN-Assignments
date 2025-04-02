5b33af7ec74340307769fe88417742b66d28a5f6
theagegupta <ayushgupta11580@gmail.com>
2025-04-03 01:27:41 +0530

    Week 3 Assignment Completed

--- Week3/react-assignment/src/components/TextUpdater.jsx
+++ Week3/react-assignment/src/components/TextUpdater.jsx
@@ -1,24 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 
-const TextUpdater = (event) => {
-    const [text, settext] = useState('');
+const TextUpdater = () => {
+    const [text, setText] = useState('');  // creating a useState variable 
 
-    const handleInput = (event)=>
-    {
-        settext(event.target.value);
-        console.log('value is',text)
-    }
+ 
+    
+
+    useEffect(() => {
+        console.log('Updated text:', text);
+    }, [text]); // This effect runs whenever 'text' changes
+
+    const handleInput = (event) => {
+        setText(event.target.value);
+    };
 
     return (
         <div className=''>
-            <label htmlFor="input" className='text-lg ml-5 mr-2'>Input some text to display : </label>
+            <label htmlFor="input" className='text-lg ml-5 mr-2'>Input some text to display: </label>
             <input
-                onChange={(event)=>{handleInput(event)}}
                 value={text}
-
-            type="text" className='py-2 px-3 rounded text-yellow-100 text-lg bg-transparent border placeholder:text-yellow-200' placeholder='Enter Something' id='input' />
+                onChange={(event)=>handleInput(event)}
+                type="text"
+                className='py-2 px-3 rounded text-yellow-100 text-lg bg-transparent border placeholder:text-yellow-200'
+                placeholder='Enter Something'
+                id='input'
+            />
         </div>
-    )
-}
+    );
+};
 
-export default TextUpdater
+export default TextUpdater;
